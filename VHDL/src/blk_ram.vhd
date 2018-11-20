a692aa101db65a78bb814b7922f124f00cafe7c1
Hunter Rawson <hunter.rawson@gtri.gatech.edu>
2018-11-19 20:45:30 -0500

    Updated RAM block for dual read/write addressing.

--- VHDL/src/blk_ram.vhd
+++ VHDL/src/blk_ram.vhd
@@ -10,7 +10,8 @@ entity blk_ram is
        -- input side
        clk      : in  std_logic;
        rst      : in  std_logic;
-       address 	: in integer;
+       wr_address 	: in integer;
+       rd_address   : in integer;
        we : in std_logic; -- Write enable
        data_i : in std_logic_vector(7 downto 0);
        data_o : out std_logic_vector(7 downto 0)
@@ -28,12 +29,12 @@ process(clk)
 begin
   if (rising_edge(clk)) then
     if (we = '1') then
-        ram(address) <= data_i;
+        ram(wr_address) <= data_i;
     end if;
     if (rst = '1') then
       data_o <= "00000000";
     else
-      data_o <= ram(address);
+      data_o <= ram(rd_address);
     end if;
   end if;
 end process;
